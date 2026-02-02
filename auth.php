1b0ad08ac074a2847ee8d3de0b17e924f278e09a
JacobRVaughn <silverarrow139@gmail.com>
2026-02-02 00:23:26 -0600

    init commit for the basic login and auth system we will be using for secure login and account creation. Uses hashed passwords for security.

---
+++ auth.php
@@ -0,0 +1,62 @@
+<?php 
+
+include 'authService.php';
+
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Login Post Form
+    if (isset($_POST['login_submit']) && isset($_POST["login_username"]) && isset($_POST["login_password"]) 
+    ) {
+
+    $username = trim($_POST["login_username"]);
+    $password = trim($_POST["login_password"]);
+
+    $result = loginUser($username,$password);
+
+    if ($result == 'success') {
+
+        $_SESSION['username'] = $username;
+        $_SESSION['logged_in'] = true;
+
+        header("Location: http://localhost/my_arcade_game/studentDashboard.html", true, 302);
+        // always follow header with exit
+        exit;
+
+    } else {
+        echo $result;
+    }
+
+    } 
+    // Signup Post Form
+    elseif (isset($_POST['signup_submit']) && isset($_POST["signup_username"]) 
+        && isset($_POST["signup_email"]) && isset($_POST["signup_password"]) && isset($_POST["signup_confirm"])
+    ) {
+
+        $signup_username = trim($_POST["signup_username"]);
+        $signup_email = trim($_POST["signup_email"]);
+        $signup_password = trim($_POST["signup_password"]);
+        $signup_confirm = trim($_POST["signup_confirm"]);
+
+        if ($signup_password != $signup_confirm) {
+            return "passwords do not match";
+        }
+
+        $result = signupUser($signup_username, $signup_email, $signup_password);
+
+        if ($result == 'success') {
+            $_SESSION['username'] = $signup_username;
+            $_SESSION['logged_in'] = true;
+
+            header("Location: http://localhost/my_arcade_game/studentDashboard.html", true, 302);
+            // always follow header with exit
+            exit;
+        } else {
+            echo $result;
+        }
+
+    }
+
+}
+
+?>
---
+++ authService.php
@@ -0,0 +1,55 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "arcade_db");
+
+// Called from auth.php to verify login information in the database
+function loginUser($username, $password) {
+    try {
+        global $conn;
+
+        $stmt = $conn->prepare("SELECT password FROM users WHERE username = ?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        $row = $result->fetch_assoc();
+
+        if (!isset($row) || empty($row)) {
+            return 'no user matches';
+        }
+
+        var_dump($row);
+
+        // var_dump($password);
+        // var_dump($row['password']);
+
+        if (password_verify($password, $row['password'])) {
+            return 'success';
+        } else {
+            return 'failure pass does not match';
+        }
+
+    } catch (Exception $e) {
+        error_log($e->getMessage());
+    }
+}
+
+//Create db user table entry for a new user
+function signupUser($username,$email,$password) {
+    global $conn;
+
+    $hashedPass = password_hash($password, PASSWORD_DEFAULT);
+
+    $stmt = $conn->prepare("INSERT INTO users (username, password, email) VALUES (?,?,?)");
+    $stmt->bind_param("sss", $username, $hashedPass, $email);
+    $stmt->execute();
+
+    if ($stmt->affected_rows > 0) {
+        return 'success';
+    } else {
+        return 'sql error has occured';
+    }
+    
+}
+
+
+?>
