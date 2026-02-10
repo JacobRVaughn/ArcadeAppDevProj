a5710d20cc24933e43a64a5a47b1e176e04a6d35
august-doss <aust.doss@gmail.com>
2026-02-10 11:38:16 -0600

    Created leaderboard.js
    Started adding JavaScript for "View All" button on leaderboard.

---
+++ leaderboard.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const openButton = document.getElementById("viewAllButton");
+    // TODO: create close button and full leaderboard elements
+
+    function openFullLeaderboard() {
+
+    }
+
+    function closeFullLeaderboard() {
+
+    }
+
+    openButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        openFullLeaderboard();
+    });
+
+    // TODO: closeButton.addEventListner(...);
+    // maybe esc button to close menu too?
+    
+
+});
